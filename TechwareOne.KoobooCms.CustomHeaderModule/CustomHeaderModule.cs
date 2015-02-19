7659011572aec207959e394914fe1fc9f272f697
Mark Sizer <mesizer@gmail.com>
2015-02-19 16:26:39 +0000

    Initial source code commit.

---
+++ TechwareOne.KoobooCms.CustomHeaderModule/CustomHeaderModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+
+/*
+	<system.webServer>
+		<modules runAllManagedModulesForAllRequests="true">
+            <add name="CustomHeaderModule" type="TechwareOne.KoobooCms.CustomHeaderModule.CustomHeaderModule" />
+        </modules>
+	</system.webServer>
+ */
+
+namespace TechwareOne.KoobooCms.CustomHeaderModule
+{
+    public class CustomHeaderModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        public void Dispose() { }
+
+        void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Remove("X-AspNetMvc-Version");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            HttpContext.Current.Response.Headers.Remove("X-KoobooCMS-Version");
+
+            // Other headers can also be added even after this event fires e.g...
+				//
+            //		HttpContext.Current.Response.Headers.Remove("X-Powered-By");
+            //		HttpContext.Current.Response.Headers.Remove("X-Powered-By-Plesk");
+            //
+            // To remove them from the response, add the following to the web.config
+			   // 
+            // <system.webServer>
+            //    <httpProtocol>
+            //       <customHeaders>
+            //          <remove name="X-Powered-By" />
+            //          <remove name="X-Powered-By-Plesk" />
+            //       </customHeaders>
+            //    </httpProtocol>
+            // </system.webServer>
+        }
+    }
+}
